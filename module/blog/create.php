83486d3ce89687dea8bf0cf1b768fc22b5778695
mainguyen01 <maintn.b20at119@stu.ptit.edu.vn>
2024-11-12 17:34:27 +0700

    fix XSS on create blog function

--- module/blog/create.php
+++ module/blog/create.php
@@ -1,8 +1,8 @@
 <?php 
     checkAuthentication(); 
     if($_SERVER['REQUEST_METHOD'] === 'POST'){
-        $title = remove_bad_character($_POST['title']);
-        $content = remove_bad_character($_POST['content']);
+        $title = htmlspecialchars($_POST['title']);
+        $content = htmlspecialchars($_POST['content']);
         $user_id = getUserId();
         $extension = pathinfo($_FILES['avatar']['name'],PATHINFO_EXTENSION);
 
